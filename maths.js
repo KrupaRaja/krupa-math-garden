1735973a0c12e3a47002b49dddc377731e59d877
KrupaRaja <36296535+KrupaRaja@users.noreply.github.com>
2021-07-26 15:06:22 +0530

    Add files via upload

--- maths.js
+++ maths.js
@@ -1,47 +1,39 @@
+
 var answer;
-var score=0;
-var backgroundImages=[];
+var score = 0;
+var backgroundImages = [];
 
-function nextQuestion(){
- const n1 = Math.floor(Math.random() * 5);
- document.getElementById('n1').innerHTML =n1;
 
- const n2 = Math.floor(Math.random() * 6);
- document.getElementById('n2').innerHTML =n2;
+function nextQuestion() {
 
- answer= n1+n2;
+    const n1 = Math.floor(Math.random() * 5);
+    document.getElementById('n1').innerHTML = n1;
+
+    const n2 = Math.floor(Math.random() * 6);
+    document.getElementById('n2').innerHTML = n2;
+    answer = n1 + n2;
 }
 
-function checkAnswer(){
-      
+function checkAnswer() {
+
     const prediction = predictImage();
+    console.log(`Answer: ${answer}, prediction: ${prediction}`);
 
-    // console.log(answer);
-    // console.log(prediction);
-
-    if (prediction == answer){
+    if (prediction == answer) {
         score++;
-       console.log(`Correct Score : ${score}`);
-       if (score <= 6) {
-        backgroundImages.push(`url('images/background${score}.svg')`);
-        document.body.style.backgroundImage = backgroundImages;
-    } 
-       else{
-
-        score=0;
-           backgroundImages=[];
-           document.body.backgroundImages=backgroundImages;
-
-           alert("Well done! Your math garden is in full bloom! Want to Start again?");
-           
-       }
-    }
-    else{
-        if(score != 0)
-        {
-            score--;
+        console.log(`Correct! Score: ${score}`);
+        if (score <= 6) {
+            backgroundImages.push(`url('images/background${score}.svg')`);
+            document.body.style.backgroundImage = backgroundImages;
+        } else {
+            alert('Well done! Your maths garden is in full bloom! Want to start again?');
+            score = 0;
+            backgroundImages = [];
+            document.body.style.backgroundImage = backgroundImages;
         }
-        console.log('Wrong ${score}');
+    } else {
+        if (score != 0) { score--; }
+        console.log(`Wrong. Score ${score}`);
 
         alert('Oops! Check your calculations and try writing the number neater next time!');
         setTimeout(function () {
@@ -49,4 +41,5 @@ function checkAnswer(){
             document.body.style.backgroundImage = backgroundImages;
         }, 1000);
     }
-    }
+
+}
